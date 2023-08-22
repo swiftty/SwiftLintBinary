cdebd916c1cd557926559233376a2608db8df21a
swiftty <62803132+swiftty@users.noreply.github.com>
2023-08-23 00:14:44 +0900

    update Package.swift in release workflow

---
+++ Package.template.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftLintPlugin",
+    products: [
+        .plugin(name: "SwiftLintPlugin", targets: ["SwiftLintPlugin"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "<url>",
+            checksum: "<checksum>"
+        ),
+        .plugin(
+            name: "SwiftLintPlugin",
+            capability: .buildTool(),
+            dependencies: [
+                "SwiftLintBinary"
+            ]
+        )
+    ]
+)
